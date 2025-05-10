626d314589b18219e3c16de2ed83f6da1c83aa59
Guruprasadh-R <guruprasadhraghavan@gmail.com>
2025-05-10 10:45:18 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,16 +4,29 @@ const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
-  password: String
+  password: String,
+  role: {
+    type: String,
+    enum: ['user', 'seller', 'admin'],
+    default: 'user'
+  }
 })
 
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next()
+
+  // Set role based on email
+  if (this.email === 'admin@recyckart.com') {
+    this.role = 'admin'
+  } else if (this.email.endsWith('@recyckart.com')) {
+    this.role = 'seller'
+  }
+
   this.password = await bcrypt.hash(this.password, 10)
   next()
 })
 
-userSchema.methods.comparePassword = function(password) {
+userSchema.methods.comparePassword = function (password) {
   return bcrypt.compare(password, this.password)
 }
 
