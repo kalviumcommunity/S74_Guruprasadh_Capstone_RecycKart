15586bac10deb7f946f9fe60e0897a02f140aee3
Raghavan Venkataraman <guruprasadhraghavan@gmail.com>
2025-05-11 20:36:16 +0530

    routes and middleware

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const userRoutes = require('./routes/userRoutes')
 const productRoutes = require('./routes/productRoutes')
+const contributionRoutes = require('./routes/contributionRoutes')
 
 dotenv.config()
 const app = express()
-const PORT = process.env.PORT || 5000
-
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err))
 
+app.use('/api/users', userRoutes)
 app.use('/api/products', productRoutes)
+app.use('/api/contributions', contributionRoutes)
 
-app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`)
 })
