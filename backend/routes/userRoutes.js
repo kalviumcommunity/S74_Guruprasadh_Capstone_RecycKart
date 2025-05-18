07d2e3a1fbdd7dc1482511595e605dcfa107da52
Raghavan Venkataraman <guruprasadhraghavan@gmail.com>
2025-05-18 12:39:33 +0530

    react installed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const { register, login } = require('../controllers/userController')
+const protect = require('../middleware/authMiddleware')
 
 router.post('/register', register)
 router.post('/login', login)
+
+// ✅ Protected Route
+router.get('/profile', protect, (req, res) => {
+  res.json({
+    message: `Welcome ${req.user.name}`,
+    email: req.user.email,
+    role: req.user.role
+  })
+})
 
 module.exports = router
